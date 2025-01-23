7f83b453d8640ccb31cf8918dd3cdadefec9c6e9
gjasper <gustavohj6@gmail.com>
2025-01-22 22:26:12 -0300

    parse any input

--- prop.py
+++ prop.py
@@ -1,19 +1,43 @@
-from pysat.formula import CNF
+from pysmt.rewritings import CNFizer
+from pysmt.shortcuts import get_env
 from pysat.solvers import Solver
-from sympy import sympify, to_cnf
+from pysmt.logics import QF_LRA
+from pysat.formula import CNF
 
 import theory
 import util
 
-def is_sat(formula, f, atom_map):
+def is_sat(formula):
 
-    f_expr = sympify(f)
-    f_cnf = str(to_cnf(f_expr))
+    atom_map = {}
 
-    s_clauses = f_cnf.replace('v', '')
+    conv = CNFizer()
+    cnf_formula = conv.convert_as_formula(formula)
+    string_formula = cnf_formula.serialize()
+
+    for atom in cnf_formula.get_atoms():
+        s_atom = str(atom)
+        atom_map[s_atom] = len(atom_map) + 1
+        string_formula = string_formula.replace(s_atom, str(atom_map[s_atom]))
+
+    theory_atoms = [atom for atom in atom_map if 'FV' not in atom]
+
+    string_formula = string_formula.replace(" | (", " | ") \
+                                    .replace(") | ", " | ") \
+                                    .replace("(((", "((") \
+                                    .replace(")))", "))") \
+                                    .replace(")) &", ") &") \
+                                    .replace("& ((", "& (") \
+                                    .replace("! ", "-")
+
+    get_env().factory.add_generic_solver("cvc5", \
+                                     "/opt/cvc5-Linux-static-1.2.0/bin/cvc5", \
+                                    [QF_LRA])
+
+    string_formula = util.trim(string_formula)
 
     clauses = []
-    for s_clause in s_clauses.split(' & '):
+    for s_clause in string_formula.split(' & '):
         clause = []
         for lit in util.trim(s_clause).split(' | '):
             clause.append(int(lit))
@@ -22,13 +46,13 @@ def is_sat(formula, f, atom_map):
     is_prop_abstraction_sat = True
 
     while is_prop_abstraction_sat:
-        cnf_formula = CNF(from_clauses = clauses)
-        with Solver(bootstrap_with = cnf_formula) as solver:
+        solver_formula = CNF(from_clauses = clauses)
+        with Solver(bootstrap_with = solver_formula) as solver:
             is_prop_abstraction_sat = solver.solve()
             if is_prop_abstraction_sat:
                 model = solver.get_model()
-                print("found propositional assignment: {}".format(model))
-                if theory.is_theory_sat(formula, atom_map, model):
+                print("prop assignment: {}".format(model))
+                if theory.is_theory_sat(formula, atom_map, theory_atoms, model):
                     break
                 else:
                     invalid_model_blocking_clause = [(-1) * l for l in model]
--- theory.py
+++ theory.py
@@ -1,20 +1,14 @@
-from pysmt.shortcuts import get_env, Not, Solver
-from pysmt.logics import QF_LRA
+from pysmt.shortcuts import Not, Solver
 
-import util
-
-def is_theory_sat(formula, atom_map, assignment):
-
-    get_env().factory.add_generic_solver("cvc5", \
-                                     "/opt/cvc5-Linux-static-1.2.0/bin/cvc5", \
-                                    [QF_LRA])
+def is_theory_sat(formula, atom_map, theory_atoms, assignment):
 
     with Solver(name = "cvc5", logic = "QF_LRA") as s:
         assertion = {}
         for atom in formula.get_atoms():
-            if atom_map[util.trim(str(atom))] in assignment:
-                assertion = assertion.And(atom) if assertion else atom
-            else:
-                assertion = assertion.And(Not(atom)) if assertion else Not(atom)
+            if str(atom) in theory_atoms:
+                if atom_map[str(atom)] in assignment:
+                    assertion = assertion.And(atom) if assertion else atom
+                else:
+                    assertion = assertion.And(Not(atom)) if assertion else Not(atom)
         s.add_assertion(assertion)
         return s.solve()
